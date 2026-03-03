144ef35b27390136c6ddd531c636d4af8ed7cfea
Dr Vijay Varadi <varadivk@gmail.com>
2026-03-03 12:33:25 +0530

    Publish static site

---
+++ out/_next/static/chunks/app/layout-6c06603cf9ba0cf8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3584:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,6050,23))},6050:function(){}},function(n){n.O(0,[387,972,971,117,744],function(){return n(n.s=3584)}),_N_E=n.O()}]);
